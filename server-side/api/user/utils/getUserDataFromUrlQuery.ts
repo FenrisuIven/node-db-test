bdd5bc8ff072628497d6a081183b9ef4aa5d386d
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2025-01-21 10:43:48 +0200

    userData validation and formatting to obj

--- server-side/api/user/utils/getUserDataFromUrlQuery.ts
+++ server-side/api/user/utils/getUserDataFromUrlQuery.ts
@@ -1,37 +1,29 @@
-import { UserData } from "../../types/UserData";
+import crypto from 'crypto';
+import { ZodError } from 'zod';
 import { ParsedUrlQuery } from "node:querystring";
 
-import crypto from 'crypto';
+import { formatErrorToMessage } from "../../utils/formatErrorToMessage";
+import { userDataSchema } from "../validation/UserSchema";
+import { UserData } from "../type/UserData";
 import { errorMessages } from "../../constants/errorMessages";
 import { Message } from "../../types/Message";
-import {formatErrorToMessage} from "../../utils/formatErrorToMessage";
 
 export function getUserDataFromUrlQuery(
     urlQuery: ParsedUrlQuery
 ) : UserData | Message {
     try {
-        console.log(Object.keys(urlQuery).length)
         if (Object.keys(urlQuery).length < 4) {
             throw new TypeError(errorMessages.TOO_FEW_ARGS_USER);
         }
         
-        // TODO: Most likely, urlQuery already contains data in needed format, this much logic is not necessary;
-        //       Try to shorthand it. It might be possible to use Object.keys(urlQuery) for this.
-
-        console.log(urlQuery);
-        
-        const values = Object.values(urlQuery);
-        const valueOfIncorrectType = values.map((elem: any) => typeof elem).slice(1)
-            .find((providedType: string) => providedType !== 'string');
-        if (valueOfIncorrectType) {
-            throw new TypeError(errorMessages.TYPE_MISMATCH);
-        }
+        const validationRes: UserData = userDataSchema.parse({
+            ...urlQuery,
+            id: Number(urlQuery.id)
+        });
         
         return {
-            id: Number(urlQuery.id),
-            username: urlQuery.user,
-            email: urlQuery.email,
-            password: crypto.createHash('md5').update(urlQuery.password).digest('hex')
+            ...validationRes,
+            password: crypto.createHash('md5').update(validationRes.password).digest('hex')
         };
     } catch (err: any) {
         return formatErrorToMessage(err);
---
+++ server-side/api/user/validation/UserSchema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const userDataSchema = z.object({
+    id: z.number(),
+    username: z.string(),
+    email: z.string(),
+    password: z.string()
+});
