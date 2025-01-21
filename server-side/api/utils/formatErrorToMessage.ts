4190bacb079ed88abe4b44b94fceab494c6f9aa7
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2025-01-21 10:44:37 +0200

    validation with zod and error handling updates

--- server-side/api/utils/formatErrorToMessage.ts
+++ server-side/api/utils/formatErrorToMessage.ts
@@ -1,11 +1,24 @@
 import { Message } from "../types/Message";
+import { z } from "zod";
 
-export const formatErrorToMessage = (err: Error): Message => {
-    let [ errorType, ...errorMessage ] = err.toString().split(' ');
-    errorType = errorType.slice(0, errorType.length - 1);
+export const formatErrorToMessage = (err: any): Message => {
+    const status = 400;
+    let code = '';
+    let msg = '';
+    
+    if (err instanceof z.ZodError) {
+        const errorData = err.issues[0];
+        code = errorData.code;
+        msg = `${errorData.message}: "${errorData.path}"`;
+    } else {
+        let [ errorType, ...errorMessage ] = err.toString().split(' ');
+        code = errorType.slice(0, errorType.length - 1);
+        msg = errorMessage.join(' ');
+    }
+    
     return {
-        status: 400,
-        code: errorType,
-        msg: errorMessage.join(' ')
-    };  
+        status, 
+        code, 
+        msg
+    };
 };
